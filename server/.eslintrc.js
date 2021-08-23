f5bd03a2cc3ce9e4e6ea1c597a97ebac4a1e557f
Epiev Alexandr <epievalex@gmail.com>
2021-08-23 13:56:51 +0400

    Base server features

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -4,11 +4,8 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
   root: true,
   env: {
     node: true,
